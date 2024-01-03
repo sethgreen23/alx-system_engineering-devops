a2bf8c92b935a95262ac39c72a2d17bc4dbb4157
sethgreen23 <dridi.chaith@gmail.com>
2024-01-03 07:21:50 -0800

    single character

--- 0x06-regular_expressions/5-beginning_and_end.rb
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 #Matching a string that starts with h and end with n
 #and can have any string character in between
-puts ARGV[0].scan(/^h\w+n$/).join
+puts ARGV[0].scan(/^h.n$/).join
