b69ef8e70f898a69628700179f5cf8b8613d08b2
sethgreen23 <dridi.chaith@gmail.com>
2024-01-03 03:37:15 -0800

    100-textme

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,7 +1,9 @@
 #!/usr/bin/env ruby
 #Matching a string that starts with h and end with n
 #and can have any string character in between
-matches = ARGV[0].scan(/.*from:([\w\+]+)\].*to:([\w\+]+)\].*flags:([\w\+\-\:]+).*/)
+re = /[\w\s\:\-\[\]+,\S]+from:([\w+]+)\][\w\s\:\-\[\]+,\S]+to:([\w+]+)\][\w\s\:\-\[\]+,\S]+flags:([\w+-\:]+)\][\w\s\:\-\[\]+,\S]+/
+reg = /.*from:([\w\+]+)\].*to:([\w\+]+)\].*flags:([\w\+\-\:]+).*/
+matches = ARGV[0].scan(re)
 if matches.any?
   timestamp, sender, message = matches[0]
   puts "#{timestamp},#{sender},#{message}"
