e7e8dc4e7bdb23f98f0fd0605eed1efcc6345e75
Dridi Chaith <dridi.chaith@gmail.com>
2024-02-23 13:36:04 -0800

    task 2 finished

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # kills a process named killmenow
 
-exec { 'kill killmenow':
-  command => 'pkill -9 killmenow',
+exec { 'killmenow':
+  command  => 'pkill -9 killmenow',
   provider => 'shell',
 }
