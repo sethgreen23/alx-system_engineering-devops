32449a5f72cbff02974a20fa952ac6039b2a3f8e
Dridi Chaith <dridi.chaith@gmail.com>
2024-02-23 13:12:14 -0800

    task1 final

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -6,6 +6,12 @@ package { 'python3-pip':
 
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
   require  => Package['python3-pip'],
 }
