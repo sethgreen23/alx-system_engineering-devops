2f4492b20f3b0c3702c1806d0aba860402406f3a
Dridi Chaith <dridi.chaith@gmail.com>
2024-02-23 11:19:55 -0800

    task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,3 +1,8 @@
+# This manifest file create a file school in tmp folder with content I love Puppet
+# persmittion '0744'
+# ownder 'www-data'
+# group 'www-data'
+
 file { 'school':
   path    => '/tmp/school',
   recurse => false,
