b717826377b4cb900005ba417ca63f9b7bbb58d3
Dridi Chaith <dridi.chaith@gmail.com>
2024-02-24 15:48:20 -0800

    task 100 done

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,12 +3,12 @@
 
 file_line { 'use private key':
   ensure => present,
-  path   => '/etc/ssh/ssh_config'
-  line   => 'IdentityFile ~/.ssh/school'
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
 }
 
 file_line { 'refuse authontication using password':
   ensure => present,
-  path   => '/etc/ssh/ssh_config'
-  line   => 'PasswordAuthentication no'
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
 }
