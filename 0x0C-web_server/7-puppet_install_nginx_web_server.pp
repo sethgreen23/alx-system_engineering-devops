a2381f4a4bba9b0d8c7e042a01656d44544187d1
Dridi Chaith <dridi.chaith@gmail.com>
2024-02-27 14:29:19 -0800

    task 0

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,14 @@
+# use private key ~/.ssh/school
+# refuse to authonticate using a password
+
+file_line { 'use private key':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
+}
+
+file_line { 'refuse authontication using password':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+}
