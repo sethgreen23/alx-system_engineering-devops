b195830d8bab56fddbcfd39dfab6e5e631fc177d
Dridi Chaith <dridi.chaith@gmail.com>
2024-02-28 09:54:24 -0800

    task 7

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,14 +1,43 @@
-# use private key ~/.ssh/school
-# refuse to authonticate using a password
+# install nginx and 
+# return string "Hello World!" when quering the root
+# rediretion must be a "301" move permanentyly
 
-file_line { 'use private key':
-  ensure => present,
-  path   => '/etc/ssh/ssh_config',
-  line   => 'IdentityFile ~/.ssh/school',
+package { 'nginx':
+  ensure => installed,
 }
 
-file_line { 'refuse authontication using password':
-  ensure => present,
-  path   => '/etc/ssh/ssh_config',
-  line   => 'PasswordAuthentication no',
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => Package['nginx'],
+}
+
+file { '/var/www/html/index.html':
+  ensure  => present,
+  content => 'Hello World!',
+}
+
+$server_config = "
+	server {
+		listen 80 default_server;
+		listen [::]:80 default_server;
+		
+		root /var/www/html;
+		index index.html
+
+		server_name _;
+
+		location /redirect_me {
+			return 301 'https://www.youtube.com';
+		}
+
+		location / {
+			try_files \$uri \$uri/404;
+		}
+}"
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => $server_config,
+  notify  => Service['nginx'],
 }
