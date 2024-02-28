9dd5a4586f63dd2d39f5c2e9d0a164f9f84e03f2
Dridi Chaith <dridi.chaith@gmail.com>
2024-02-28 11:23:09 -0800

    task 7

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,24 +2,45 @@
 # return string "Hello World!" when quering the root
 # rediretion must be a "301" move permanentyly
 
+exec { 'apt-update':
+  command     => '/usr/bin/apt-get update',
+  refreshonly => true,
+  before      => Package['nginx'],
+}
+
 package { 'nginx':
   ensure => installed,
 }
 
 service { 'nginx':
-  ensure  => 'running',
+  ensure  => running,
   enable  => true,
   require => Package['nginx'],
 }
 
-file_line { 'rewrite redirect'
-  ensure   => 'present',
-  path     => '/etc/nginx/sites-available/default',
-  after    => 'listen 80 default_server;',
-  line     => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+file { '/var/www/html/index.html':
+  ensure  => present,
+  content => 'Hello World!',
 }
 
-file { '/var/www/html/index.html':
-  ensure  => 'present',
-  content => 'Hello World!',
+$server_config = "
+	server {
+		listen 80 default_server;
+		listen [::]:80 default_server;
+		
+		root /var/www/html;
+		index index.html
+
+		server_name _;
+		rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
+
+		location / {
+			try_files \$uri \$uri/404;
+		}
+}"
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => $server_config,
+  notify  => Service['nginx'],
 }
