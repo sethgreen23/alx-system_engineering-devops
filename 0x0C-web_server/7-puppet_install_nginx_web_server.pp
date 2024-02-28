a54953d47fa00defa87202924e407495042b6cc0
Dridi Chaith <dridi.chaith@gmail.com>
2024-02-28 11:19:50 -0800

    kjl

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -7,19 +7,19 @@ package { 'nginx':
 }
 
 service { 'nginx':
-  ensure  => running,
+  ensure  => 'running',
   enable  => true,
   require => Package['nginx'],
 }
 
 file_line { 'rewrite redirect'
-  ensure   => present,
+  ensure   => 'present',
   path     => '/etc/nginx/sites-available/default',
   after    => 'listen 80 default_server;',
   line     => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
 file { '/var/www/html/index.html':
-  ensure  => present,
+  ensure  => 'present',
   content => 'Hello World!',
 }
