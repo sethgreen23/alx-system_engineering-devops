cf87bdda0f692373bc93515b9a9d579a9466a0bd
Dridi Chaith <dridi.chaith@gmail.com>
2024-02-28 11:08:30 -0800

    task 7

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -32,10 +32,7 @@ $server_config = "
 		index index.html
 
 		server_name _;
-
-		location /redirect_me {
-			return 301 'https://www.youtube.com';
-		}
+		rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4;
 
 		location / {
 			try_files \$uri \$uri/404;
