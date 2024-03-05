d8dd2ff08529edeb8b5c2235a7be9c65e247abb0
root <dridi.chaith@gmail.com>
2024-03-05 12:11:34 -0800

    task 02

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,62 +1,23 @@
-# Ensure nginx package is installed
-package { 'nginx':
-  ensure => installed,
+# updating apt
+exec { 'update':
+  command  => 'sudo apt-get -y update',
+  provider => shell,
 }
 
-# Configure nginx root page
-file { '/var/www/html/index.nginx-debian.html':
-  ensure  => present,
-  content => "Hello World!\n",
+# installing nginx
+-> exec { 'install':
+  command  => 'sudo apt-get -y install nginx',
+  provider => shell,
 }
 
-# Configure redirect_me
-file { '/etc/nginx/sites-enabled/default':
-  ensure  => present,
-  content => "
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-
-    root /var/www/html;
-    index index.html index.htm index.nginx-debian.html;
-
-    server_name _;
-
-    location /redirect_me {
-        return 301 http://youtube.com;
-    }
-
-    # Other location directives if needed
-}
-",
+# adds the header to the file
+-> exec { 'X-Served-By':
+    command => 'sudo sed -i "/listen 80 default_server;/a         add_header X-Served-By $(hostname);
+  " /etc/nginx/sites-available/default',
+  provider  => shell,
 }
 
-# Configure custom 404 page
-file { '/usr/share/nginx/html/404.html':
-  ensure  => present,
-  content => "Ceci n'est pas une page\n",
+# restarting nginx
+-> exec { 'service -y nginx':
+  command => '/usr/sbin/service nginx restart',
 }
-
-# Add custom HTTP response header to nginx.conf
-file_line { 'custom_header_nginx_conf':
-  ensure => present,
-  line   => '    add_header X-Served-By $HOSTNAME;',
-  path   => '/etc/nginx/nginx.conf',
-  match  => '^http {',
-}
-
-# Add custom HTTP response header to default site configuration
-file_line { 'custom_header_default_conf':
-  ensure => present,
-  line   => '    add_header X-Served-By $HOSTNAME;',
-  path   => '/etc/nginx/sites-enabled/default',
-  match  => '^server {',
-}
-
-# Restart nginx service
-service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  subscribe => [File['/etc/nginx/nginx.conf'], File['/etc/nginx/sites-enabled/default']],
-}
-
