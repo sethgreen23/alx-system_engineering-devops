5ad2d011c3f7acc59630fec0555d979842175be4
Dridi Chaith <dridi.chaith@gmail.com>
2024-03-25 15:47:36 +0100

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,24 +4,29 @@ import sys
 import requests
 
 
-emp_id = sys.argv[1]
-request_f = f'https://jsonplaceholder.typicode.com/users/{emp_id}'
-emp_req = requests.get(request_f)
-
-if emp_req.status_code == 200:
-    resp_emp = emp_req.json()
-    name = resp_emp.get('name')
-    request_f = f'https://jsonplaceholder.typicode.com/todos?userId={emp_id}'
-    todos_req = requests.get(request_f)
-    if todos_req.status_code == 200:
-        resp_todos = todos_req.json()
-        completed_todos_count = 0
-        all_c = len(resp_todos)
-        completed_list = []
-        for todo in resp_todos:
-            if todo.get('completed'):
-                completed_list.append(todo)
-        completed_t_c = len(completed_list)
-        print(f'Employee {name} done with tasks({completed_t_c}/{all_c}):')
-        for todo in completed_list:
-            print(f'\t {todo.get("title")}')
+id = sys.argv[1]
+if id:
+    try:
+        id = int(id)
+        request_f = f'https://jsonplaceholder.typicode.com/users/{id}'
+        emp_req = requests.get(request_f)
+        if emp_req.status_code == 200:
+            resp_emp = emp_req.json()
+            name = resp_emp.get('name')
+            req_f = f'https://jsonplaceholder.typicode.com/todos?userId={id}'
+            todos_req = requests.get(req_f)
+            if todos_req.status_code == 200:
+                resp_todos = todos_req.json()
+                completed_todos_count = 0
+                all_c = len(resp_todos)
+                completed_list = []
+                for todo in resp_todos:
+                    if todo.get('completed'):
+                        completed_list.append(todo)
+                completed_t_c = len(completed_list)
+                print(f'Employee {name} '
+                      f'done with tasks({completed_t_c}/{all_c}):')
+                for todo in completed_list:
+                    print(f'\t {todo.get("title")}')
+    except ValueError:
+        pass
