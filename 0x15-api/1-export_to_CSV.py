9bd8966e756bf34fcd497b5fd8ffcb6c02422ef4
Dridi Chaith <dridi.chaith@gmail.com>
2024-03-25 18:35:53 +0100

    task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -21,34 +21,17 @@ if __name__ == "__main__":
                 todos_req = requests.get(req_f)
                 if todos_req.status_code == 200:
                     resp_todos = todos_req.json()
-                    completed_todos_count = 0
-                    all_c = len(resp_todos)
-                    completed_list = []
-                    for todo in resp_todos:
-                        if todo.get('completed'):
-                            completed_list.append(todo)
-                    completed_t_c = len(completed_list)
-                    print(f'Employee {name} is '
-                          f'done with tasks({completed_t_c}/{all_c}):')
-                    for todo in completed_list:
-                        print(f'\t {todo.get("title")}')
-                    fn = ["USER_ID",
-                          "USERNAME",
-                          "TASK_COMPLETED_STAT",
-                          "TASK_TITLE"]
                     filename = f'{id}.csv'
                     with open(filename, mode='w') as csvfile:
-                        writer = csv.DictWriter(csvfile, fieldnames=fn,
-                                                doublequote=False)
+                        writer = csv.writer(csvfile,
+                                            quoting=csv.QUOTE_ALL)
                         # writer.writeheader()
                         for todo in resp_todos:
                             completed = todo.get('completed')
                             title = todo.get('title')
-                            writer.writerow({
-                                "USER_ID": str(id),
-                                "USERNAME": str(username),
-                                "TASK_COMPLETED_STAT": str(completed),
-                                "TASK_TITLE": str(title)
-                                })
+                            writer.writerow([str(id),
+                                             str(username),
+                                             str(completed),
+                                             str(title)])
         except ValueError:
             pass
