0566e2740971fb007687757f629dc7a3f7493635
Dridi Chaith <dridi.chaith@gmail.com>
2024-04-09 21:06:13 +0100

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
-"""Total number of subscribers"""
-
-
+"""Number of subscribers"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Number of subscribers for certain subredit"""
+    """
+        Number of subscribers
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+
     headers = requests.utils.default_headers()
-    headers = {"User-Agent": "ChangeMeClient/0.1 by sethgreen2015"}
-    r = requests.get(f'https://www.reddit.com/r/{subreddit}/about.json',
-                     headers=headers, allow_redirects=False)
-    if r.status_code == 200:
-        subscribers = r.json().get('data', {}).get('subscribers')
-        if subscribers is None:
-            return (0)
-        else:
-            return (subscribers)
-    else:
-        return (0)
+    headers['User-Agent'] = 'My User Agent 0.0.1'
+
+    res = requests.get(url, headers=headers, allow_redirects=False).json()
+    subscribers = res.get('data', {}).get('subscribers')
+    if not subscribers:
+        return 0
+    return subscribers
