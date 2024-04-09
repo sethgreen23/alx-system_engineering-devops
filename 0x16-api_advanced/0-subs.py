663707e822bbbbe73751b5ec0fe99217e5fd1274
Dridi Chaith <dridi.chaith@gmail.com>
2024-04-09 21:02:35 +0100

    task 10

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,9 +2,12 @@
 """Total number of subscribers"""
 
 
+import requests
+
+
 def number_of_subscribers(subreddit):
     """Number of subscribers for certain subredit"""
-    import requests
+    headers = requests.utils.default_headers()
     headers = {"User-Agent": "ChangeMeClient/0.1 by sethgreen2015"}
     r = requests.get(f'https://www.reddit.com/r/{subreddit}/about.json',
                      headers=headers, allow_redirects=False)
